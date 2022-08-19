c82c717ab1734ee8c96c2dbee43ef401f25e97ba
pvesagas <39418141+pvesagas@users.noreply.github.com>
2022-08-20 00:50:29 +0800

    setup tailwind, browser sync plugin, rename welcome blade to index blade

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+mix.browserSync('127.0.0.1:8000')
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -13,4 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss')
+    ]);
